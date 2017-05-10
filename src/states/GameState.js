efb97c58d30181abb8524a8a75b0b86811a11b8e
Allan Leon <aleon.107@outlook.com>
2017-05-09 23:19:48 -0400

    Added preload and update functions to GameState

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,7 +1,17 @@
 class GameState extends Phaser.State {
+	preload() {
+		// This function will be executed at the beginning
+    // Here's where we load the images and sounds
+	}
 
 	create() {
+		// This function is called after the preload function
+    // Here we set up the game, display sprites, etc.
+	}
 
+	update() {
+		// This function is called 60 times per second
+    // It contains the game's logic
 	}
 
 }
